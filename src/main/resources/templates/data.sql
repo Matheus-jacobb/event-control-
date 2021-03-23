7bb5e76b5aa91d730381c718d4a909f777154558
Matheus Jacob <70240646+Matheus-jacobb@users.noreply.github.com>
2021-03-23 10:12:21 -0300

    [ADD] data.sql

---
+++ src/main/resources/templates/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO tb_client (name, description, place, startDate, endDate, startTime, endTime, email) 
+VALUES (
+    'Introdução Java', 
+    'Palestra introdutoria sobre tecnologia java, crescimento, mercado e inovação', 
+    'FACENS',
+    '25/12/2022',
+    '25/12/2022',
+    '09:00:00',
+    '12:00:00',
+    'suporte@java.com.br'
+    );
