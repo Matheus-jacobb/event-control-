9853fbd7d1cbf3c893ffc83044906f98e0a0c80a
Matheus Jacob <70240646+Matheus-jacobb@users.noreply.github.com>
2021-03-28 20:57:03 -0300

    add delete and put

--- src/main/resources/templates/data.sql
+++ src/main/resources/templates/data.sql
@@ -1,10 +1,10 @@
-INSERT INTO tb_client (name, description, place, startDate, endDate, startTime, endTime, email) 
+INSERT INTO TB_Event (name, description, place, startDate, endDate, startTime, endTime, email) 
 VALUES (
     'Introdução Java', 
     'Palestra introdutoria sobre tecnologia java, crescimento, mercado e inovação', 
     'FACENS',
-    '25/12/2022',
-    '25/12/2022',
+    '2022-12-25',
+    '2022-12-25',
     '09:00:00',
     '12:00:00',
     'suporte@java.com.br'
