77439f28f247419e00a811d0f3ebaf3a4c5f0bd2
Matheus Jacob <70240646+Matheus-jacobb@users.noreply.github.com>
2021-03-29 09:49:51 -0300

    insert data on prod profile

--- src/main/resources/templates/data.sql
+++ src/main/resources/templates/data.sql
@@ -14,7 +14,7 @@ VALUES (
 VALUES (
     'Introdução Python', 
     'Palestra introdutoria sobre tecnologia python, crescimento, mercado e inovação', 
-    'FACENS',
+    'UNIP',
     '2022-12-26',
     '2022-12-26',
     '09:00:00',
@@ -26,12 +26,36 @@ VALUES (
 VALUES (
     'Introdução C#', 
     'Palestra introdutoria sobre tecnologia C#, crescimento, mercado e inovação', 
-    'FACENS',
+    'ESAMC',
     '2022-12-27',
     '2022-12-27',
     '09:00:00',
     '12:00:00',
     'suporte@csharp.com.br'
+    );
+
+        INSERT INTO TB_Event (name, description, place, startDate, endDate, startTime, endTime, email) 
+VALUES (
+    'Introdução POWER BI', 
+    'Palestra introdutoria sobre tecnologia PowerBI, crescimento, mercado e inovação', 
+    'UNISO',
+    '2023-01-05',
+    '2023-01-05',
+    '09:00:00',
+    '12:00:00',
+    'suporte@powerbi.com.br'
+    );
+
+    INSERT INTO TB_Event (name, description, place, startDate, endDate, startTime, endTime, email) 
+VALUES (
+    'Introdução C', 
+    'Palestra introdutoria sobre tecnologia Linguagem C, crescimento, mercado e inovação', 
+    'Parque Tecnologico',
+    '2023-01-06',
+    '2023-01-06',
+    '09:00:00',
+    '12:00:00',
+    'suporte@linguagemc.com.br'
     );
 
     
