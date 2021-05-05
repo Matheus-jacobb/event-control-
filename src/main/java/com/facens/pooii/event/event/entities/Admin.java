719e1bb0b6fb6260cfcf5fcddfbb94a03ec3ff21
Matheus Jacob <matheusbendel@hotmail.com>
2021-05-05 00:04:15 -0300

    add (entities): add classes UML

---
+++ src/main/java/com/facens/pooii/event/event/entities/Admin.java
@@ -0,0 +1,16 @@
+package com.facens.pooii.event.event.entities;
+
+public class Admin extends BaseUser{
+    private String phoneNumber;
+
+    
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    
+}
