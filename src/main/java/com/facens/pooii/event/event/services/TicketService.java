c2667784b71aa8f2ff1d330563fa5984aa20d89b
Matheus Jacob <matheusbendel@hotmail.com>
2021-06-15 21:38:12 -0300

    update(Ticket): limitValidation to buy a ticket

--- src/main/java/com/facens/pooii/event/event/services/TicketService.java
+++ src/main/java/com/facens/pooii/event/event/services/TicketService.java
@@ -46,19 +46,50 @@ public class TicketService {
         Event event = eventService.getEventById(idEvent);
         Attend attend = attendService.getAttendById(dto.getAttendId());
         Ticket ticket = new Ticket(dto);
+        
+        Boolean isFull = limitValidation(event, ticket.getType());
 
-        ticket.setDate(Instant.now());
-        ticket.setAttend(attend);
-        ticket.setEvent(event);
-        if (ticket.getType() == Type.PAYED) {
-            ticket.getAttend().setBalance(dueTicket(ticket));
-            ticket.getEvent().ticketPayedDecrement();
-        } else {
-            ticket.getEvent().ticketFreeDecrement();
+        try{
+            if (isFull)
+                throw new Exception();
+
+            
+            ticket.setDate(Instant.now());
+            ticket.setAttend(attend);
+            ticket.setEvent(event);
+            
+            isPayed(ticket);
+            ticket = ticketRepository.save(ticket);
+            return ticket;
         }
-        ticket = ticketRepository.save(ticket);
-        return ticket;
+        catch (Exception e){
+             if (ticket.getType() == Type.FREE)
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Free tickets is over");
+
+             else
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Payed tickets is over");
+        }
+
     }
+
+    public void deleteTicket(Long id) {
+
+        try {
+            Ticket ticket = ticketRepository.getOne(id);
+            ticket.getAttend().setBalance(ticket.getAttend().getBalance() + ticket.getPrice());
+
+            isPayed(ticket);
+
+            ticket = ticketRepository.save(ticket);
+            ticketRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Ticket not found");
+        }
+    }
+
+    /**
+     * Validators
+     */
 
     public double dueTicket(Ticket ticket) {
         double balance = 0;
@@ -90,21 +121,26 @@ public class TicketService {
         }
     }
 
-    public void deleteTicket(Long id) {
+    public void isPayed(Ticket ticket){
+        if (ticket.getType() == Type.PAYED) {
+            ticket.getAttend().setBalance(dueTicket(ticket));
+            ticket.getEvent().ticketPayedDecrement();
+        } else {
+            ticket.getEvent().ticketFreeDecrement();
+        }
+    }
 
-        try {
-            Ticket ticket = ticketRepository.getOne(id);
-            ticket.getAttend().setBalance(ticket.getAttend().getBalance() + ticket.getPrice());
-            
-            if (ticket.getType() == Type.PAYED) 
-                ticket.getEvent().ticketPayedIncrement();
-             else 
-                ticket.getEvent().ticketFreeIncrement();
+    public boolean limitValidation(Event event, Type type){
+        if(event.getAmountFreeTickets() == 0 && type==Type.FREE){
+            return true;
+        }
 
-            ticket = ticketRepository.save(ticket);
-            ticketRepository.deleteById(id);
-        } catch (EmptyResultDataAccessException e) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Ticket not found");
+        else if(event.getAmountPayedTickets() == 0 && type==Type.PAYED){
+            return true;
+        }
+
+        else{
+            return false;
         }
     }
 
